a7d85784a556b52df1a9e82e6bddb67d02248575
masafumi-tech <83009688+masafumi-tech@users.noreply.github.com>
2021-04-29 13:28:43 +0900

    practice1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue" message="お元気ですか" />
       <ColorfulMessage color="pink" message="元気です" />
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(´・ω・｀)</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const ColorfulMessage = (props) => {
   const { color, message, fontSize } = props;
-  console.log(props);
   const contentStyle = {
     color,
     fontSize
